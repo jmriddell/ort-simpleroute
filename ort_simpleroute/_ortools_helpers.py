78f4af2edef5d9f288aada7f4e600d41c0a638f5
j.molina.riddell <jmriddell@protonmail.ch>
2020-07-28 16:21:12 -0400

    implement _CallbackManager copy all methods related to callback tracking to _CallbackManager

--- ort_simpleroute/_ortools_helpers.py
+++ ort_simpleroute/_ortools_helpers.py
@@ -112,22 +112,11 @@ class _CallbackIndexTracker:
         return callback_or_index in self._callbacks_and_indexes
 
 
-_add_dimension_error = RuntimeError(
-    "Failed to add dimension, " + "is possible that the name provided is already used."
-)
-
-
-class ConvenientModel:
-    """Class to group the routing manager and model, and functions with side effects."""
-
-    def __init__(self, num_nodes: int, num_vehicles: int = 1, depot: int = 0):
-        self.manager: Manager = pywrapcp.RoutingIndexManager(
-            num_nodes, num_vehicles, depot
-        )
-        self.model: Model = pywrapcp.RoutingModel(self.manager)
-        self._callback_tracker = _CallbackIndexTracker()
-        
-        self._deliveries_enabled = False
+class _CallbackManager:
+    def __init__(self, model, manager):
+        self.manager = manager
+        self.model = model
+        self._callback_index_tracker = _CallbackIndexTracker()
 
     def _register_transit_callback(self, distance_callback: NDistanceCallback) -> int:
         """
@@ -138,7 +127,6 @@ class ConvenientModel:
         possible to use the same callback function for multiple purposes like for more
         than one vehicle but not all.
         """
-
         index_distance_callback: IDistanceCallback = node2index_distance_callback(
             self.manager, distance_callback
         )
@@ -163,7 +151,7 @@ class ConvenientModel:
         return unary_callback_index
 
     def _register_callback(self, callback):
-        if self._callback_tracker.is_present(callback):
+        if self._callback_index_tracker.is_present(callback):
             raise ValueError("Callback already present.")
         argument_count = _argument_count(callback)
         if argument_count == 1:
@@ -172,25 +160,101 @@ class ConvenientModel:
             callback_index = self._register_transit_callback(callback)
         else:
             raise ValueError("Callback needs to have 1 or 2 arguments.")
-        self._callback_tracker.add_callback(callback, callback_index)
+        self._callback_index_tracker.add_callback(callback, callback_index)
 
     def callback_to_index(self, callback):
         """Get index of callback and register if not already present."""
-        index = self._callback_tracker.get_index(callback)
+        index = self._callback_index_tracker.get_index(callback)
         if index is None:
             self._register_callback(callback)
-            index = self._callback_tracker.get_index(callback)
+            index = self._callback_index_tracker.get_index(callback)
+            assert index is not None
+        return index
+
+
+_add_dimension_error = RuntimeError(
+    "Failed to add dimension, " + "is possible that the name provided is already used."
+)
+
+
+class ConvenientModel:
+    """Class to group the routing manager and model, and functions with side effects."""
+
+    def __init__(self, num_nodes: int, num_vehicles: int = 1, depot: int = 0):
+        self.manager: Manager = pywrapcp.RoutingIndexManager(
+            num_nodes, num_vehicles, depot
+        )
+        self.model: Model = pywrapcp.RoutingModel(self.manager)
+
+        self._callback_manager = _CallbackManager(self.model, self.manager)
+
+        self._deliveries_enabled = False
+
+    def _register_transit_callback(self, distance_callback: NDistanceCallback) -> int:
+        """
+        Register a callback, assign an internal index to it, and return its index.
+
+        The internal index is returned in order to later tell the model how to use the
+        callback, which now will be referenced by it's index. That way it will be
+        possible to use the same callback function for multiple purposes like for more
+        than one vehicle but not all.
+        """
+        index_distance_callback: IDistanceCallback = node2index_distance_callback(
+            self.manager, distance_callback
+        )
+        transit_callback_index = self.model.RegisterTransitCallback(
+            index_distance_callback
+        )
+        return transit_callback_index
+
+    def _register_unary_callback(self, demand_callback):
+        """
+        Register a callback, assign an internal index to it, and return its index.
+
+        Same as register_transit_callback but the callback thakes just one argument
+        and returns a value associated to a node instead of a path.
+        """
+        index_distance_callback = node2index_demand_callback(
+            self.manager, demand_callback
+        )
+        unary_callback_index = self.model.RegisterUnaryTransitCallback(
+            index_distance_callback
+        )
+        return unary_callback_index
+
+    def _register_callback(self, callback):
+        if self._callback_index_tracker.is_present(callback):
+            raise ValueError("Callback already present.")
+        argument_count = _argument_count(callback)
+        if argument_count == 1:
+            callback_index = self._register_unary_callback(callback)
+        elif argument_count == 2:
+            callback_index = self._register_transit_callback(callback)
+        else:
+            raise ValueError("Callback needs to have 1 or 2 arguments.")
+        self._callback_index_tracker.add_callback(callback, callback_index)
+
+    def callback_to_index(self, callback):
+        """Get index of callback and register if not already present."""
+        index = self._callback_index_tracker.get_index(callback)
+        if index is None:
+            self._register_callback(callback)
+            index = self._callback_index_tracker.get_index(callback)
             assert index is not None
         return index
 
     def set_global_arc_cost(self, distance_callback):
         assert _argument_count(distance_callback) == 2
-        transit_callback_index = self.callback_to_index(distance_callback)
+        transit_callback_index = self._callback_manager.callback_to_index(
+            distance_callback
+        )
         self.model.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
     def set_vehicle_arc_cost(self, distance_callback, vehicle_num: int):
         assert _argument_count(distance_callback) == 2
-        transit_callback_index = self.callback_to_index(distance_callback)
+        transit_callback_index = self._callback_manager.callback_to_index(
+            distance_callback
+        )
         self.model.SetArcCostEvaluatorOfVehicle(transit_callback_index, vehicle_num)
 
     def add_dimension(
@@ -202,7 +266,7 @@ class ConvenientModel:
         fix_start_cumul_to_zero: bool = True,
     ):
         """https://developers.google.com/optimization/reference/python/constraint_solver/pywrapcp#adddimension"""
-        callback_index = self.callback_to_index(callback)
+        callback_index = self._callback_manager.callback_to_index(callback)
         success = self.model.AddDimension(
             callback_index,
             slack_max,  # capacity slack
@@ -222,7 +286,7 @@ class ConvenientModel:
         slack_max=0,
         fix_start_cumul_to_zero: bool = True,
     ):
-        callback_index = self.callback_to_index(callback)
+        callback_index = self._callback_manager.callback_to_index(callback)
         success = self.model.AddDimensionWithVehicleCapacity(
             callback_index,
             slack_max,  # capacity slack
@@ -252,7 +316,7 @@ class ConvenientModel:
     def _enable_deliveries(self):
         if self._deliveries_enabled:
             return
-        self._cumul_dim = self.add_dimension(lambda x, y: 1, maxsize, '_cumul')
+        self._cumul_dim = self.add_dimension(lambda x, y: 1, maxsize, "_cumul")
         self._deliveries_enabled = True
 
     def add_delivery_request(self, from_node, to_node):
@@ -261,17 +325,16 @@ class ConvenientModel:
         delivery_index = self.manager.NodeToIndex(to_node)
         self.model.AddPickupAndDelivery(pickup_index, delivery_index)
         self.model.solver().Add(
-            self.model.VehicleVar(pickup_index) == self.model.VehicleVar(
-                delivery_index
-            )
+            self.model.VehicleVar(pickup_index) == self.model.VehicleVar(delivery_index)
         )
         self.model.solver().Add(
-            self._cumul_dim.CumulVar(pickup_index) <=
-            self._cumul_dim.CumulVar(delivery_index)
+            self._cumul_dim.CumulVar(pickup_index)
+            <= self._cumul_dim.CumulVar(delivery_index)
         )
 
     def allow_drop_of_node(self, node, penalty):
         self.model.AddDisjunction([self.manager.NodeToIndex(node)], penalty)
+
 
 def solution_sequence(
     rmod: ConvenientModel, solution: Solution
