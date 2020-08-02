7f4921a23ca5c79ccf6ebe2ff4e0384a7a58bbef
j.molina.riddell <jmriddell@protonmail.ch>
2020-08-01 23:24:01 -0400

    rename folder

--- ort_simpleroute/_examples/test_same_behavior.py
+++ ort_simpleroute/_examples/test_same_behavior.py
@@ -1,29 +1,33 @@
 from unittest import TestCase
 
-from ort_simpleroute.google_examples import (
+from ort_simpleroute._examples import (
     capacity_constraint,
     re_capacity_constraint,
     pickup_delivery,
     re_pickup_delivery,
 )
 
-from io import StringIO 
+from io import StringIO
 import sys
+
 
 class Capturing(list):
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._stringio = StringIO()
         return self
+
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
+        del self._stringio  # free up some memory
         sys.stdout = self._stdout
+
 
 def capture_lines(function):
     with Capturing() as output:
         function()
     return output
+
 
 class CompareOutputsTestCase(TestCase):
     def test_capacity_constraint(self):
