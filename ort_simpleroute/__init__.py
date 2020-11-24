2c1833202465afd32c515fc0c1602b8ada0d3a97
j.molina.riddell <jmriddell@protonmail.ch>
2020-11-24 18:28:51 -0300

    add package docstring for ort_simpleroute

--- ort_simpleroute/__init__.py
+++ ort_simpleroute/__init__.py
@@ -1,3 +1,3 @@
-"""Public imports."""
+"""An abstraction layer around ortools route optimization modules."""
 from .ortools_helpers import RouteOptimizer, solution_sequence
 from . import fss_enum as fss
